e14d17a0606ff6ad63a1a98bf6d4c97e7393c841
corgan hejijun <corganhejijun@gmail.com>
2016-06-02 12:22:34 +0800

    user register ok, can check username and work id duplicate

--- FiProcess/registerForm.py
+++ FiProcess/registerForm.py
@@ -1,23 +1,43 @@
 # -*- coding: utf-8 -*-
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, render
 from django.forms import ModelForm
 from django.template.context import RequestContext
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+
 from .models import Stuff
 
 
 class RegisterForm(ModelForm):
+
     class Meta:
         model = Stuff
         fields = ['username', 'name', 'workId', 'phoneNumber', 'department', 'icbcCard', 'ccbCard', 'password']
 
     def get(self, request):
-        return render_to_response('FiProcess/register.html', RequestContext(request, {'form': self, }))
+        form = RegisterForm(
+            initial={'username': '', 'name': '', 'workId': '', 'phoneNumber': '', 'icbcCard': '', 'ccbCard': ''}
+        )
+        return render_to_response('FiProcess/register.html', RequestContext(request, {'form': form, 'register_success': True}))
 
     def post(self, request):
         inst = Stuff()
         form = RegisterForm(request.POST, instance=inst)
         if form.is_valid():
-            form.save()
-            return render_to_response('FiProcess/login.html', RequestContext(request, {'form': form, 'register_success': True}))
+            # username and work id duplication check
+            username = Stuff.objects.filter(username=inst.username)
+            workId = Stuff.objects.filter(workId=inst.workId)
+            message = ""
+            if username:
+                message = u"用户" + inst.username + u"已存在"
+            if workId:
+                message = u"工号" + inst.workId + u"已注册"
+            if len(message) > 0:
+                return render(request, 'FiProcess/register.html', {'form': form, 'message': message})
+            inst.save()
+            inst.stuffcheck_set.create()
+            messages.add_message(request, messages.INFO, inst.username)
+            return HttpResponseRedirect(reverse('login'))
         else:
-            return render_to_response('FiProcess/debug.html', RequestContext(request, {'form': form, 'register_success': False}))
+            return render(request, 'FiProcess/register.html', {'form': form, 'register_success': False})
