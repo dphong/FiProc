6c8a3b3de732bf96948ee2befb419f0e0db97052
corgan hejijun <corganhejijun@gmail.com>
2016-06-15 23:28:54 +0800

    some view change mistakes at index

--- FiProcess/Form/IndexForm.py
+++ FiProcess/Form/IndexForm.py
@@ -80,17 +80,15 @@ class IndexForm(forms.Form):
                 raise Exception("字段内容错误")
             staff = self.queryStaff(userInfoForm.cleaned_data['username'], userInfoForm.cleaned_data['password'])
         except Exception, e:
-            return render_to_response('FiProcess/index.html',
-                RequestContext(request, {'userInfoForm': userInfoForm, 'saveFailedMsg': str(e)})
-            )
+            messages.add_message(request, messages.ERROR, '保存失败:' + str(e))
+            return self.render(request, userInfoForm)
         staff.phoneNumber = userInfoForm.cleaned_data['phoneNumber']
         staff.icbcCard = userInfoForm.cleaned_data['icbcCard']
         staff.ccbCard = userInfoForm.cleaned_data['ccbCard']
         staff.save()
         userInfoForm.password = ''
-        return render_to_response('FiProcess/index.html',
-            RequestContext(request, {'userInfoForm': userInfoForm, 'saveSuccess': True})
-        )
+        messages.add_message(request, messages.SUCCESS, '保存成功')
+        return self.render(request, userInfoForm)
 
     def saveNewPassword(self, request):
         userInfoForm = self.getUserInfoForm(request)
@@ -100,14 +98,12 @@ class IndexForm(forms.Form):
         try:
             staff = self.queryStaff(request.session['username'], originPsw, "原密码错误")
         except Exception, e:
-            return render_to_response('FiProcess/index.html',
-                RequestContext(request, {'userInfoForm': userInfoForm, 'saveFailedMsg': str(e)})
-            )
+            messages.add_message(request, messages.ERROR, '保存失败:' + str(e))
+            return self.render(request, userInfoForm)
         staff.password = make_password(newPsw)
         staff.save()
-        return render_to_response('FiProcess/index.html',
-            RequestContext(request, {'userInfoForm': userInfoForm, 'saveSuccess': True})
-        )
+        messages.add_message(request, messages.SUCCESS, '保存成功')
+        return self.render(request, userInfoForm)
 
     def approveStaffCheck(self, request):
         userInfoForm = self.getUserInfoForm(request)
@@ -119,10 +115,27 @@ class IndexForm(forms.Form):
                 staff.staff.delete()
         elif 'userCheckOK'in request.POST:
             querySet.delete()
+        messages.add_message(request, messages.SUCCESS, '保存成功')
+        return self.render(request, userInfoForm)
+
+    def render(self, request, userInfoForm):
+        if request.user.is_authenticated():
+            return render_to_response('FiProcess/index.html',
+                RequestContext(request, {'userInfoForm': userInfoForm,
+                    'orderList': self.getOrderList(request),
+                    'userCheckList': StaffCheck.objects.all(), 'is_sysAdmin': True}))
+        querySet = StaffCheck.objects.filter(staff__username__exact=request.session['username'])
+        if querySet.count() > 0:
+            return render_to_response('FiProcess/index.html',
+                RequestContext(request, {'userInfoForm': userInfoForm, 'unCheckStaff': True}))
+        signList = self.getSignList(request)
+        if len(signList) > 0:
+            return render_to_response('FiProcess/index.html',
+                RequestContext(request, {'userInfoForm': userInfoForm,
+                    'orderList': self.getOrderList(request), 'signList': signList}))
         return render_to_response('FiProcess/index.html',
             RequestContext(request, {'userInfoForm': userInfoForm,
-                'userCheckList': StaffCheck.objects.all(), 'is_sysAdmin': True, 'saveSuccess': True})
-        )
+                'orderList': self.getOrderList(request)}))
 
     def streamStageChange(self, stream):
         querySet = SignRecord.objects.filter(stream__id=stream.id)
@@ -155,6 +168,7 @@ class IndexForm(forms.Form):
             return HttpResponseRedirect(reverse('index', args={'newstream'}))
         username = request.session['username']
         querySet = FiStream.objects.filter(applicante__username=username)
+        userInfoForm = self.getUserInfoForm(request)
         for item in querySet:
             if ("order" + str(item.id)) in request.POST:
                 request.session['orderId'] = item.id
@@ -164,7 +178,8 @@ class IndexForm(forms.Form):
                 if 'orderId' in request.session:
                     del request.session['orderId']
                 messages.add_message(request, messages.SUCCESS, item.projectName + u'报销单删除成功')
-                return HttpResponseRedirect(reverse('index', args={''}))
+                userInfoForm.currentTab = 'order'
+                return self.render(request, userInfoForm)
         querySet = SignRecord.objects.filter(signer__username__exact=username)
         for item in querySet:
             if ("sign" + str(item.id)) in request.POST:
@@ -177,19 +192,21 @@ class IndexForm(forms.Form):
                 item.save()
                 item.stream.currentStage = 'refused'
                 item.stream.save()
-                return HttpResponseRedirect(reverse('index', args={''}))
+                userInfoForm.currentTab = 'signList'
+                return self.render(request, userInfoForm)
             if ("signOk" + str(item.id)) in request.POST:
                 item.signed = True
                 item.discript = request.POST['signOkDiscript']
                 item.save()
+                userInfoForm.currentTab = 'signList'
                 try:
                     item.stream.currentStage = self.streamStageChange(item.stream)
                     item.stream.save()
                 except Exception, e:
-                    messages.add_message(request, messages.SUCCESS, e)
-                    return HttpResponseRedirect(reverse('index', args={''}))
+                    messages.add_message(request, messages.ERROR, str(e))
+                    return self.render(request, userInfoForm)
                 messages.add_message(request, messages.SUCCESS, u'报销单审核成功')
-                return HttpResponseRedirect(reverse('index', args={''}))
+                return self.render(request, userInfoForm)
         return self.logout(request)
 
     def sortOrder(self, stream):
@@ -235,29 +252,13 @@ class IndexForm(forms.Form):
         return signList
 
     def get(self, request):
+        userInfoForm = self.getUserInfoForm(request)
         if 'orderId' in request.session:
             del request.session['orderId']
         if 'username' not in request.session:
             messages.add_message(request, messages.ERROR, '登录失败!')
             return HttpResponseRedirect(reverse('login'))
-        userInfoForm = self.getUserInfoForm(request)
-        if request.user.is_authenticated():
-            return render_to_response('FiProcess/index.html',
-                RequestContext(request, {'userInfoForm': userInfoForm,
-                    'orderList': self.getOrderList(request),
-                    'userCheckList': StaffCheck.objects.all(), 'is_sysAdmin': True}))
-        querySet = StaffCheck.objects.filter(staff__username__exact=request.session['username'])
-        if querySet.count() > 0:
-            return render_to_response('FiProcess/index.html',
-                RequestContext(request, {'userInfoForm': userInfoForm, 'unCheckStaff': True}))
-        signList = self.getSignList(request)
-        if len(signList) > 0:
-            return render_to_response('FiProcess/index.html',
-                RequestContext(request, {'userInfoForm': userInfoForm,
-                    'orderList': self.getOrderList(request), 'signList': signList}))
-        return render_to_response('FiProcess/index.html',
-            RequestContext(request, {'userInfoForm': userInfoForm,
-                'orderList': self.getOrderList(request)}))
+        return self.render(request, userInfoForm)
 
     def getStaffFromRequest(self, request):
         username = request.session['username']
