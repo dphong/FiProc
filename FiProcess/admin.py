ac24849c5ffebc1515863ca9faf1d39230b03d23
corgan hejijun <corganhejijun@gmail.com>
2016-06-24 22:26:12 +0800

    shrink department chief select number

--- FiProcess/admin.py
+++ FiProcess/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
-from .models import Department, SchoolMaster
+from .models import Department, SchoolMaster, Staff
+from django import forms
+
+
+class DepartmentSelectForm(forms.ModelForm):
+    class Meta:
+        model = Department
+        fields = ['name', 'secretary', 'chief']
+
+    def __init__(self, *args, **kwargs):
+        super(DepartmentSelectForm, self).__init__(*args, **kwargs)
+        self.fields['secretary'].queryset = Staff.objects.filter(department__id=self.instance.id)
+        self.fields['chief'].queryset = Staff.objects.filter(department__id=self.instance.id)
 
 
 class DepartmentAdmin(admin.ModelAdmin):
     list_display = ('name', 'secretary', 'chief')
+    form = DepartmentSelectForm
 
 
 class SchoolMasterAdmin(admin.ModelAdmin):
